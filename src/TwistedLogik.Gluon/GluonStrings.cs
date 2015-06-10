a65c44957511043257a13c0c6ce20841c32975aa
Eric Mellino <erme@microsoft.com>
2015-06-10 01:26:22 -0700

    Compiling Gluon

---
+++ src/TwistedLogik.Gluon/GluonStrings.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using TwistedLogik.Nucleus.Text;
+
+namespace TwistedLogik.Gluon
+{
+    /// <summary>
+    /// Contains Gluon's string resources.
+    /// </summary>
+    public static class GluonStrings
+    {
+        /// <summary>
+        /// Initializes the GluonStrings type.
+        /// </summary>
+        static GluonStrings()
+        {
+#if NETCORE
+            var asm = typeof(GluonStrings).GetTypeInfo().Assembly;
+#else
+            var asm = Assembly.GetExecutingAssembly();
+#endif
+            using (var stream = asm.GetManifestResourceStream("TwistedLogik.Gluon.Resources.Strings.xml"))
+            {
+                StringDatabase.LoadFromStream(stream);
+            }
+        }
+
+        private static readonly LocalizationDatabase StringDatabase = new LocalizationDatabase();
+
+        public static readonly StringResource CouldNotLoadFunction        = new StringResource(StringDatabase, "COULD_NOT_LOAD_FUNCTION");
+        public static readonly StringResource LoadedOpenGLVersion         = new StringResource(StringDatabase, "LOADED_OPENGL_VERSION");
+        public static readonly StringResource UnloadedOpenGL              = new StringResource(StringDatabase, "UNLOADED_OPENGL");
+        public static readonly StringResource OpenGLNotInitialized        = new StringResource(StringDatabase, "OPENGL_NOT_INITIALIZED");
+        public static readonly StringResource OpenGLAlreadyInitialized    = new StringResource(StringDatabase, "OPENGL_ALREADY_INITIALIZED");
+        public static readonly StringResource FunctionNotProvidedByDriver = new StringResource(StringDatabase, "FUNCTION_NOT_PROVIDED_BY_DRIVER");
+        public static readonly StringResource MissingRequiredExtension    = new StringResource(StringDatabase, "MISSING_REQUIRED_EXTENSION");
+        public static readonly StringResource DriverReturnedNullPointer   = new StringResource(StringDatabase, "DRIVER_RETURNED_NULL_POINTER");
+    }
+}
